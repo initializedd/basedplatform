1aaf0608ef609b3ceeb9cc32bcf5040cebb2dc82
initializedd <119122691+initializedd@users.noreply.github.com>
2024-06-30 21:22:22 +0100

    CPUID encrypted memory features support (#52)

---
+++ basedplatform/based/platform/x86/detail/cpu_encrypted_memory_features.hpp
@@ -0,0 +1,123 @@
+#pragma once
+
+#include <cstdint>
+#include <utility>
+
+#include "cpu_id.hpp"
+
+// Encrypted Memory Capabilities
+// https://en.wikipedia.org/wiki/CPUID#EAX=8000001Fh:_Encrypted_Memory_Capabilities
+
+namespace based::platform::x86::detail {
+    enum class cpu_encrypted_memory_feature : std::uint8_t {
+        sme,
+        sev,
+        page_flush_msr,
+        sev_es,
+        sev_snp,
+        vmpl,
+        rmpquery,
+        vmpl_sss,
+        secure_tsc,
+        tsc_aux_virtualization,
+        hw_enf_cache_coh,
+        host_64_bit, // 64BitHost
+        restricted_injection,
+        alternate_injection,
+        debug_virt,
+        prevent_host_ibs,
+        vte,
+        vmgexit_parameter,
+        virtual_tom_msr,
+        ibs_virt_guest_ctl,
+        pmc_virt_guest_ctl,
+        rmpread,
+        // bits 23:22 reserved
+        vmsa_reg_prot          = 24u,
+        smt_protection,
+        secure_avic,
+        allowed_sev_features,
+        svsm_comm_page_msr,
+        nested_virt_snp_msr,
+        hv_in_use_wr_allowed,
+        ibpb_on_entry
+    }; // enum class cpu_encrypted_memory_feature : std::uint8_t
+
+    enum class cpu_encrypted_memory_feature_mask : std::uint32_t {
+        sme                    = 1u << std::to_underlying(cpu_encrypted_memory_feature::sme),
+        sev                    = 1u << std::to_underlying(cpu_encrypted_memory_feature::sev),
+        page_flush_msr         = 1u << std::to_underlying(cpu_encrypted_memory_feature::page_flush_msr),
+        sev_es                 = 1u << std::to_underlying(cpu_encrypted_memory_feature::sev_es),
+        sev_snp                = 1u << std::to_underlying(cpu_encrypted_memory_feature::sev_snp),
+        vmpl                   = 1u << std::to_underlying(cpu_encrypted_memory_feature::vmpl),
+        rmpquery               = 1u << std::to_underlying(cpu_encrypted_memory_feature::rmpquery),
+        vmpl_sss               = 1u << std::to_underlying(cpu_encrypted_memory_feature::vmpl_sss),
+        secure_tsc             = 1u << std::to_underlying(cpu_encrypted_memory_feature::secure_tsc),
+        tsc_aux_virtualization = 1u << std::to_underlying(cpu_encrypted_memory_feature::tsc_aux_virtualization),
+        hw_enf_cache_coh       = 1u << std::to_underlying(cpu_encrypted_memory_feature::hw_enf_cache_coh),
+        host_64_bit            = 1u << std::to_underlying(cpu_encrypted_memory_feature::host_64_bit), // 64BitHost
+        restricted_injection   = 1u << std::to_underlying(cpu_encrypted_memory_feature::restricted_injection),
+        alternate_injection    = 1u << std::to_underlying(cpu_encrypted_memory_feature::alternate_injection),
+        debug_virt             = 1u << std::to_underlying(cpu_encrypted_memory_feature::debug_virt),
+        prevent_host_ibs       = 1u << std::to_underlying(cpu_encrypted_memory_feature::prevent_host_ibs),
+        vte                    = 1u << std::to_underlying(cpu_encrypted_memory_feature::vte),
+        vmgexit_parameter      = 1u << std::to_underlying(cpu_encrypted_memory_feature::vmgexit_parameter),
+        virtual_tom_msr        = 1u << std::to_underlying(cpu_encrypted_memory_feature::virtual_tom_msr),
+        ibs_virt_guest_ctl     = 1u << std::to_underlying(cpu_encrypted_memory_feature::ibs_virt_guest_ctl),
+        pmc_virt_guest_ctl     = 1u << std::to_underlying(cpu_encrypted_memory_feature::pmc_virt_guest_ctl),
+        rmpread                = 1u << std::to_underlying(cpu_encrypted_memory_feature::rmpread),
+        vmsa_reg_prot          = 1u << std::to_underlying(cpu_encrypted_memory_feature::vmsa_reg_prot),
+        smt_protection         = 1u << std::to_underlying(cpu_encrypted_memory_feature::smt_protection),
+        secure_avic            = 1u << std::to_underlying(cpu_encrypted_memory_feature::secure_avic),
+        allowed_sev_features   = 1u << std::to_underlying(cpu_encrypted_memory_feature::allowed_sev_features),
+        svsm_comm_page_msr     = 1u << std::to_underlying(cpu_encrypted_memory_feature::svsm_comm_page_msr),
+        nested_virt_snp_msr    = 1u << std::to_underlying(cpu_encrypted_memory_feature::nested_virt_snp_msr),
+        hv_in_use_wr_allowed   = 1u << std::to_underlying(cpu_encrypted_memory_feature::hv_in_use_wr_allowed),
+        ibpb_on_entry          = 1u << std::to_underlying(cpu_encrypted_memory_feature::ibpb_on_entry)
+    }; // enum class cpu_encrypted_memory_feature_mask : std::uint32_t
+
+    [[nodiscard]]
+    cpu_encrypted_memory_feature_mask supported_cpu_encrypted_memory_features(const std::uint32_t max_leaf) noexcept {
+        auto result = cpu_encrypted_memory_feature_mask{};
+
+        if (max_leaf >= 0x8000001F) {
+            using cpu_encrypted_memory_feature;
+            const auto leaf8 = cpu_id(0x8000001F, 0u);
+
+            // eax register
+            if (leaf8.eax_bit( 0u)) result |= sme;
+            if (leaf8.eax_bit( 1u)) result |= sev;
+            if (leaf8.eax_bit( 2u)) result |= page_flush_msr;
+            if (leaf8.eax_bit( 3u)) result |= sev_es;
+            if (leaf8.eax_bit( 4u)) result |= sev_snp;
+            if (leaf8.eax_bit( 5u)) result |= vmpl;
+            if (leaf8.eax_bit( 6u)) result |= rmpquery;
+            if (leaf8.eax_bit( 7u)) result |= vmpl_sss;
+            if (leaf8.eax_bit( 8u)) result |= secure_tsc;
+            if (leaf8.eax_bit( 9u)) result |= tsc_aux_virtualization;
+            if (leaf8.eax_bit(10u)) result |= hw_enf_cache_coh;
+            if (leaf8.eax_bit(11u)) result |= host_64_bit;
+            if (leaf8.eax_bit(12u)) result |= restricted_injection;
+            if (leaf8.eax_bit(13u)) result |= alternate_injection;
+            if (leaf8.eax_bit(14u)) result |= debug_virt;
+            if (leaf8.eax_bit(15u)) result |= prevent_host_ibs;
+            if (leaf8.eax_bit(16u)) result |= vte;
+            if (leaf8.eax_bit(17u)) result |= vmgexit_parameter;
+            if (leaf8.eax_bit(18u)) result |= virtual_tom_msr;
+            if (leaf8.eax_bit(19u)) result |= ibs_virt_guest_ctl;
+            if (leaf8.eax_bit(20u)) result |= pmc_virt_guest_ctl;
+            if (leaf8.eax_bit(21u)) result |= rmpread;
+            if (leaf8.eax_bit(24u)) result |= vmsa_reg_prot;
+            if (leaf8.eax_bit(25u)) result |= smt_protection;
+            if (leaf8.eax_bit(26u)) result |= secure_avic;
+            if (leaf8.eax_bit(27u)) result |= allowed_sev_features;
+            if (leaf8.eax_bit(28u)) result |= svsm_comm_page_msr;
+            if (leaf8.eax_bit(29u)) result |= nested_virt_snp_msr;
+            if (leaf8.eax_bit(30u)) result |= hv_in_use_wr_allowed;
+            if (leaf8.eax_bit(31u)) result |= ibpb_on_entry;
+        }
+
+        return result;
+    }
+} // namespace based::platform::x86::detail
+
