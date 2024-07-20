e234f7164456cc24f281b7e527fe00ba2c10af7e
initializedd <119122691+initializedd@users.noreply.github.com>
2024-07-20 15:27:22 +0100

    CPUID extended timing features support (#72)

---
+++ basedplatform/based/platform/x86/detail/cpu_extended_timing_features.hpp
@@ -0,0 +1,54 @@
+#pragma once
+
+#include <cstdint>
+#include <utility>
+
+#include "cpu_id.hpp"
+
+// CPU Extended Timing Features
+// https://en.wikipedia.org/wiki/CPUID#EAX=7,_ECX=2:_Extended_Features
+
+namespace based::platform::x86::detail {
+    enum class cpu_extended_timing_feature : std::uint8_t {
+        psfd,
+        ipred_ctrl,
+        rrsba_ctrl,
+        ddpd_u,
+        bhi_ctrl,
+        mcdt_no,
+        uc_lock
+        // bits 31:7 reserved
+    }; // enum class cpu_extended_timing_feature : std::uint8_t
+
+    enum class cpu_extended_timing_feature_mask : std::uint32_t {
+        psfd       = 1u << std::to_underlying(cpu_extended_timing_feature::psfd),
+        ipred_ctrl = 1u << std::to_underlying(cpu_extended_timing_feature::ipred_ctrl),
+        rrsba_ctrl = 1u << std::to_underlying(cpu_extended_timing_feature::rrsba_ctrl),
+        ddpd_u     = 1u << std::to_underlying(cpu_extended_timing_feature::ddpd_u),
+        bhi_ctrl   = 1u << std::to_underlying(cpu_extended_timing_feature::bhi_ctrl),
+        mcdt_no    = 1u << std::to_underlying(cpu_extended_timing_feature::mcdt_no),
+        uc_lock    = 1u << std::to_underlying(cpu_extended_timing_feature::uc_lock)
+    }; // enum class cpu_extended_timing_feature_mask : std::uint32_t
+
+    [[nodiscard]]
+    cpu_extended_timing_feature_mask supported_cpu_extended_timing_features(const std::uint32_t max_leaf) noexcept {
+        auto result = cpu_extended_timing_feature_mask{};
+
+        if (max_leaf >= 7u) {
+            using cpu_extended_timing_feature;
+            const auto leaf7 = cpu_id(7u, 2u);
+
+            // edx register
+            if (leaf7.edx_bit(0u)) result |= psfd;
+            if (leaf7.edx_bit(1u)) result |= ipred_ctrl;
+            if (leaf7.edx_bit(2u)) result |= rrsba_ctrl;
+            if (leaf7.edx_bit(3u)) result |= ddpd_u;
+            if (leaf7.edx_bit(4u)) result |= bhi_ctrl;
+            if (leaf7.edx_bit(5u)) result |= mcdt_no;
+            if (leaf7.edx_bit(6u)) result |= uc_lock;
+        }
+
+        return result;
+    }
+} // namespace based::platform::x86::detail
+
